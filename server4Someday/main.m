4c7ab4cb19d4bd067ad26e189d9f15cc1ecc0744
yuanrui <yuanrui@yuanruitekiMacBook-Pro-2.local>
2014-07-09 13:41:33 +0800

    Initial Commit

---
+++ server4Someday/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  server4Someday
+//
+//  Created by yuanrui on 14-7-9.
+//  Copyright (c) 2014年 yuanrui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
